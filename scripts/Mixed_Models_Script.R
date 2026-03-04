90795f17192187515f416995c5c1793c34a85d34
George01010102 <uss22pfu@uea.ac.uk>
2026-03-04 12:58:55 +0000

    Fixed ggplot fill by group by converting group to a factor when using fill

--- scripts/Mixed_Models_Script.R
+++ scripts/Mixed_Models_Script.R
@@ -4,4 +4,37 @@ library(here)
 benzo_data <- read.csv(here("data","benzo_data.csv"))
 getwd()
 view(benzo_data)
-#stuff
+
+
+ggplot(benzo_data, aes(x = benzo_um, y = detox_exp)) +
+  geom_point(alpha = 0.6) +
+  labs(
+    x = "Toxin concentration (µM)",
+    y = "Detoxification capacity"
+  ) +
+  geom_smooth(method = "lm", se = TRUE)
+#plots data ignoring group structure
+#Graph shows positive relationship
+
+
+ggplot(benzo_data, aes(x = benzo_um, y = detox_exp, colour = group)) +
+  geom_point(alpha = 0.6) +
+  labs(
+    x = "Toxin concentration (µM)",
+    y = "Detoxification capacity"
+  ) +
+  theme(legend.position = "right")
+#plots data points colored by experimental batch
+#shows that groups differ substantially in baseline detox capacity
+
+
+ggplot(benzo_data, aes(x = group, y = detox_exp, fill = factor(group))) +
+  geom_boxplot() +
+  labs(
+    x = "Experimental batch",
+    y = "Detoxification capacity"
+  ) +
+  theme(legend.position = "none")
+#boxplot shows how detoxification capacity differs between experimental batches
+
+summarise(benzo_data)
